56392469bf09cd0ec66177a05d28711cdf040ef8
Aditya Singh <aadi950690@gmail.com>
2026-08-12 16:58:19 +0530

    feat: initialize project configuration with ESLint setup and Next.js standalone build support

--- StockX-frontend/next.config.ts
+++ StockX-frontend/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   ...(process.env.BUILD_STANDALONE === 'true' ? { output: 'standalone' } : {}),
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
